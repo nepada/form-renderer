bb5848a81275fa9b4e0bf5498510c3d2cbbb8354
Petr Morávek <petr@pada.cz>
2017-08-06 12:21:37 +0200

    DI extension

---
+++ src/Bridges/FormRendererDI/FormRendererExtension.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * This file is part of the nepada/form-renderer.
+ * Copyright (c) 2017 Petr Morávek (petr@pada.cz)
+ */
+
+declare(strict_types = 1);
+
+namespace Nepada\Bridges\FormRendererDI;
+
+use Nepada\FormRenderer\Bootstrap3Renderer;
+use Nepada\FormRenderer\IBootstrap3RendererFactory;
+use Nepada\FormRenderer\ITemplateRendererFactory;
+use Nepada\FormRenderer\InvalidStateException;
+use Nepada\FormRenderer\TemplateRenderer;
+use Nette\DI\CompilerExtension;
+
+
+class FormRendererExtension extends CompilerExtension
+{
+
+    /** @var mixed[] */
+    public $defaults = [
+        'default' => [
+            'imports' => [
+                TemplateRenderer::DEFAULT_FORM_BLOCKS_TEMPLATE_FILE,
+            ],
+        ],
+        'bootstrap3' => [
+            'mode' => Bootstrap3Renderer::MODE_BASIC,
+            'imports' => [],
+        ],
+    ];
+
+
+    /**
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingReturnTypeHint
+     */
+    public function loadConfiguration()
+    {
+        $config = $this->validateConfig($this->defaults);
+        $container = $this->getContainerBuilder();
+
+        $defaultRendererFactory = $container->addDefinition($this->prefix('defaultRendererFactory'))
+            ->setImplement(ITemplateRendererFactory::class);
+        foreach ($config['default']['imports'] as $templateFile) {
+            $defaultRendererFactory->addSetup('importTemplate', [$templateFile]);
+        }
+
+        $bootstrap3RendererFactory = $container->addDefinition($this->prefix('bootstrap3RendererFactory'))
+            ->setImplement(IBootstrap3RendererFactory::class);
+        foreach ($config['bootstrap3']['imports'] as $templateFile) {
+            $bootstrap3RendererFactory->addSetup('importTemplate', [$templateFile]);
+        }
+        if ($config['bootstrap3']['mode'] === Bootstrap3Renderer::MODE_HORIZONTAL) {
+            $bootstrap3RendererFactory->addSetup('setHorizontalMode');
+        } elseif ($config['bootstrap3']['mode'] === Bootstrap3Renderer::MODE_INLINE) {
+            $bootstrap3RendererFactory->addSetup('setInlineMode');
+        } elseif ($config['bootstrap3']['mode'] === Bootstrap3Renderer::MODE_BASIC) {
+            $bootstrap3RendererFactory->addSetup('setBasicMode');
+        } else {
+            throw new InvalidStateException("Unsupported bootstrap 3 renderer mode '{$config['bootstrap3']['mode']}'.");
+        }
+    }
+
+}
---
+++ tests/Bridges/FormRendererDI/FormRendererExtensionTest.phpt
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Test: Nepada\Bridges\FormRendererDI\FormRendererExtensionTest.
+ *
+ * This file is part of the nepada/security-annotations.
+ * Copyright (c) 2017 Petr Morávek (petr@pada.cz)
+ */
+
+declare(strict_types = 1);
+
+namespace NepadaTests\Bridges\FormRendererDI;
+
+use Nepada;
+use Nepada\FormRenderer;
+use NepadaTests\TestCase;
+use Nette;
+use Tester\Assert;
+
+
+require_once __DIR__ . '/../../bootstrap.php';
+
+
+class FormRendererExtensionTest extends TestCase
+{
+
+    /** @var Nette\DI\Container */
+    private $container;
+
+
+    public function testServices(): void
+    {
+        Assert::type(FormRenderer\ITemplateRendererFactory::class, $this->container->getService('formRenderer.defaultRendererFactory'));
+        Assert::type(FormRenderer\IBootstrap3RendererFactory::class, $this->container->getService('formRenderer.bootstrap3RendererFactory'));
+    }
+
+    public function testRendering(): void
+    {
+        $form = new Nette\Forms\Form();
+
+        $defaultRenderer = $this->container->getByType(FormRenderer\ITemplateRendererFactory::class)->create();
+        Assert::same('FORM', $defaultRenderer->render($form));
+
+        $bootstrap3Renderer = $this->container->getByType(FormRenderer\IBootstrap3RendererFactory::class)->create();
+        Assert::same('FORM horizontal', $bootstrap3Renderer->render($form));
+    }
+
+    protected function setUp(): void
+    {
+        $configurator = new Nette\Configurator;
+        $configurator->setTempDirectory(TEMP_DIR);
+        $configurator->setDebugMode(true);
+        $configurator->addParameters(['fixturesDir' => __DIR__ . '/fixtures']);
+        $configurator->addConfig(__DIR__ . '/fixtures/config.neon');
+        $this->container = $configurator->createContainer();
+    }
+
+}
+
+
+\run(new FormRendererExtensionTest());
