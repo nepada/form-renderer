93992815053240b665cb5b3cfebb30c89cbb9a11
Petr Morávek <petr@pada.cz>
2019-11-03 19:47:38 +0100

    FormRendererExtension: use config schema

--- src/Bridges/FormRendererDI/FormRendererExtension.php
+++ src/Bridges/FormRendererDI/FormRendererExtension.php
@@ -7,34 +7,50 @@ use Nepada\FormRenderer\Bootstrap3Renderer;
 use Nepada\FormRenderer\IBootstrap3RendererFactory;
 use Nepada\FormRenderer\ITemplateRendererFactory;
 use Nepada\FormRenderer\TemplateRenderer;
+use Nette;
 use Nette\DI\CompilerExtension;
 
 class FormRendererExtension extends CompilerExtension
 {
 
-    /** @var mixed[] */
-    public $defaults = [
-        'default' => [
-            'imports' => [
+    public function getConfigSchema(): Nette\Schema\Schema
+    {
+        $imports = Nette\Schema\Expect::listOf('string');
+
+        $default = Nette\Schema\Expect::structure([
+            'imports' => $imports->default([
                 TemplateRenderer::DEFAULT_FORM_BLOCKS_TEMPLATE_FILE,
-            ],
-        ],
-        'bootstrap3' => [
-            'mode' => Bootstrap3Renderer::MODE_BASIC,
-            'imports' => [],
-        ],
-    ];
+            ]),
+        ]);
+
+        $bootstrap3Mode = Nette\Schema\Expect::anyOf(
+            Bootstrap3Renderer::MODE_BASIC,
+            Bootstrap3Renderer::MODE_INLINE,
+            Bootstrap3Renderer::MODE_HORIZONTAL
+        )->default(Bootstrap3Renderer::MODE_BASIC);
+
+        $bootstrap3 = Nette\Schema\Expect::structure([
+            'imports' => $imports,
+            'mode' => $bootstrap3Mode,
+        ]);
+
+        return Nette\Schema\Expect::structure([
+            'default' => $default,
+            'bootstrap3' => $bootstrap3,
+        ]);
+    }
 
     public function loadConfiguration(): void
     {
-        $config = $this->validateConfig($this->defaults);
+        /** @var \stdClass $config */
+        $config = $this->getConfig();
         $container = $this->getContainerBuilder();
 
         $defaultRendererFactory = $container->addFactoryDefinition($this->prefix('defaultRendererFactory'))
             ->setImplement(ITemplateRendererFactory::class);
 
         $defaultRendererFactoryResultDefinition = $defaultRendererFactory->getResultDefinition();
-        foreach ($config['default']['imports'] as $templateFile) {
+        foreach ($config->default->imports as $templateFile) {
             $defaultRendererFactoryResultDefinition->addSetup('importTemplate', [$templateFile]);
         }
 
@@ -42,17 +58,17 @@ class FormRendererExtension extends CompilerExtension
             ->setImplement(IBootstrap3RendererFactory::class);
 
         $bootstrap3RendererFactoryResultDefinition = $bootstrap3RendererFactory->getResultDefinition();
-        foreach ($config['bootstrap3']['imports'] as $templateFile) {
+        foreach ($config->bootstrap3->imports as $templateFile) {
             $bootstrap3RendererFactoryResultDefinition->addSetup('importTemplate', [$templateFile]);
         }
-        if ($config['bootstrap3']['mode'] === Bootstrap3Renderer::MODE_HORIZONTAL) {
+        if ($config->bootstrap3->mode === Bootstrap3Renderer::MODE_HORIZONTAL) {
             $bootstrap3RendererFactoryResultDefinition->addSetup('setHorizontalMode');
-        } elseif ($config['bootstrap3']['mode'] === Bootstrap3Renderer::MODE_INLINE) {
+        } elseif ($config->bootstrap3->mode === Bootstrap3Renderer::MODE_INLINE) {
             $bootstrap3RendererFactoryResultDefinition->addSetup('setInlineMode');
-        } elseif ($config['bootstrap3']['mode'] === Bootstrap3Renderer::MODE_BASIC) {
+        } elseif ($config->bootstrap3->mode === Bootstrap3Renderer::MODE_BASIC) {
             $bootstrap3RendererFactoryResultDefinition->addSetup('setBasicMode');
         } else {
-            throw new \InvalidArgumentException("Unsupported bootstrap 3 renderer mode '{$config['bootstrap3']['mode']}'.");
+            throw new \InvalidArgumentException("Unsupported bootstrap 3 renderer mode '{$config->bootstrap3->mode}'.");
         }
     }
 
