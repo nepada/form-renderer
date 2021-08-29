d0c074a75c77843667b2e14e99548c653943d716
Petr Morávek <petr@pada.cz>
2021-08-29 21:01:13 +0200

    Migrate to composer runtime api v2

--- tests/PHPStan/conditional.config.tests.php
+++ tests/PHPStan/conditional.config.tests.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types = 1);
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Nette\Bridges\ApplicationLatte\LatteFactory;
 
 $config = [];
@@ -14,9 +16,7 @@ if (interface_exists(LatteFactory::class)) {
     ];
 }
 
-// Read nette/forms version from bundled package.json, this should be replaced by Composer v2 runtime package version API
-$netteFormsVersion = json_decode((string) file_get_contents(__DIR__ . '/../../vendor/nette/forms/package.json'))->version;
-if (version_compare($netteFormsVersion, '3.0.5', '<')) {
+if (InstalledVersions::satisfies(new VersionParser(), 'nette/forms', '<3.0.5')) {
     // false positive, fixed in nette/forms 3.0.5
     $config['parameters']['ignoreErrors'][] = [
         'message' => '~Parameter #1 \\$value of method Nette\\\\Forms\\\\Controls\\\\MultiChoiceControl::setDisabled\\(\\) expects array<bool>\\|bool, array<int, string> given~',
@@ -25,7 +25,7 @@ if (version_compare($netteFormsVersion, '3.0.5', '<')) {
     ];
 }
 
-if (version_compare($netteFormsVersion, '3.1.2', '>=')) {
+if (InstalledVersions::satisfies(new VersionParser(), 'nette/forms', '>=3.1.2')) {
     // method available since nette/forms 3.1.2
     $config['parameters']['ignoreErrors'][] = [
         'message' => '~Call to function method_exists\\(\\) with Nette\\\\Forms\\\\Form and \'initialize\' will always evaluate to true~',
