965791b8ebe203f8303c57705f646c9ed07bd3b3
Petr Morávek <petr@pada.cz>
2023-02-04 15:33:09 +0100

    Allow nette/utils 4

--- tests/PHPStan/conditional.config.php
+++ tests/PHPStan/conditional.config.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types = 1);
 
+use Nette\Localization\Translator;
+
 $config = [];
 
 if (version_compare(Latte\Engine::VERSION, '3.0', '<')) {
@@ -38,6 +40,16 @@ if (version_compare(Latte\Engine::VERSION, '3.0', '<')) {
 if (PHP_VERSION_ID >= 8_00_00) {
     $config['parameters']['ignoreErrors'][] = [
         'message' => '~^Missing native return typehint mixed$~',
+        'path' => '../../src/FormRenderer/Filters/SafeTranslateFilter.php',
+        'count' => 1,
+    ];
+}
+
+// Detecting version from composer installed versions does not work, because it gets hijacked by phpstan.phar
+$isNetteUtils4 = (new ReflectionMethod(Translator::class, 'translate'))->getParameters()[0]->getType() instanceof ReflectionUnionType;
+if ($isNetteUtils4) {
+    $config['parameters']['ignoreErrors'][] = [
+        'message' => '~^Parameter \\#1 \\$message of method Nette\\\\Localization\\\\Translator\\:\\:translate\\(\\) expects string\\|Stringable, mixed given\\.$~',
         'path' => '../../src/FormRenderer/Filters/SafeTranslateFilter.php',
         'count' => 1,
     ];
